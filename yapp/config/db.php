9a60d9049187d0d88dfc85b04ad2e7ebf9de2140
Quadrosh <quadrosh@gmail.com>
2017-09-06 16:28:32 +0300

    sql user

--- yapp/config/db.php
+++ yapp/config/db.php
@@ -3,8 +3,8 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=snslogistic',
-    'username' => 'tb303',
-    'password' => '303',
+    'username' => 'snslogistsql',
+    'password' => '0E7l8X1r',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
